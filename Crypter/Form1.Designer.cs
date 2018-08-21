755267f6fcbb7bcf3860ae2bcd897f8b3112307f
VGzsysadm <victor.gz@sysadm.es>
2018-08-22 00:48:44 +0200

    Pull source

---
+++ Crypter/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Crypter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.resum = new System.Windows.Forms.Label();
+            this.Start = new System.Windows.Forms.Button();
+            this.incDirs = new System.Windows.Forms.CheckBox();
+            this.Dir = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(249, 214);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(337, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // resum
+            // 
+            this.resum.AutoSize = true;
+            this.resum.Location = new System.Drawing.Point(249, 197);
+            this.resum.Name = "resum";
+            this.resum.Size = new System.Drawing.Size(0, 13);
+            this.resum.TabIndex = 1;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(249, 266);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 2;
+            this.Start.Text = "start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // incDirs
+            // 
+            this.incDirs.AutoSize = true;
+            this.incDirs.Location = new System.Drawing.Point(252, 243);
+            this.incDirs.Name = "incDirs";
+            this.incDirs.Size = new System.Drawing.Size(129, 17);
+            this.incDirs.TabIndex = 3;
+            this.incDirs.Text = "Include subdirectories";
+            this.incDirs.UseVisualStyleBackColor = true;
+            // 
+            // Dir
+            // 
+            this.Dir.Location = new System.Drawing.Point(168, 174);
+            this.Dir.Name = "Dir";
+            this.Dir.Size = new System.Drawing.Size(75, 23);
+            this.Dir.TabIndex = 4;
+            this.Dir.Text = "Examine";
+            this.Dir.UseVisualStyleBackColor = true;
+            this.Dir.Click += new System.EventHandler(this.Dir_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(249, 174);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(337, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "%UserProfile%\\Downloads";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Dir);
+            this.Controls.Add(this.incDirs);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.resum);
+            this.Controls.Add(this.progressBar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label resum;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.CheckBox incDirs;
+        private System.Windows.Forms.Button Dir;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Crypter/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Crypter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Enabling default path
+        public static
+        string pathWithEnv = @"%USERPROFILE%\Downloads";
+        string fullpath = Environment.ExpandEnvironmentVariables(pathWithEnv);
+
+        public void ShowFiles(String pathFrom, Boolean incDirs)
+        {
+            // If subdir is active
+            if (incDirs)
+                {
+                string[] allfiles = Directory.GetFiles(pathFrom, "*.*", SearchOption.AllDirectories);
+                // Set minimum value for bar
+                progressBar.Minimum = 0;
+                // Set max length bar value related to files found
+                progressBar.Maximum = allfiles.Length;
+                // Action for each file
+                foreach (var file in allfiles)
+                {
+                    // Increase bar for each file found
+                    progressBar.Increment(1);
+                    FileInfo info = new FileInfo(file);
+                }
+                    // Sump up files
+                    resum.Text = "# of Files Read = " + progressBar.Value.ToString();
+                    System.Windows.Forms.MessageBox.Show("Files found: " + allfiles.Length.ToString(), "Message");
+                    // Restart bar
+                    progressBar.Value = 0;
+                }
+            else
+            {
+                // Get files
+                string[] files = Directory.GetFiles(pathFrom);
+                // Set minimum value for bar
+                progressBar.Minimum = 0;
+                // Set max length bar value related to files found
+                progressBar.Maximum = files.Length;
+                foreach (var file in files)
+                {
+                    // Increase bar for each file found
+                    progressBar.Increment(1);
+                    FileInfo info = new FileInfo(file);
+                }
+                // Sump up files
+                resum.Text = "# of Files Read = " + progressBar.Value.ToString();
+                System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                // Restart bar
+                progressBar.Value = 0;
+            }
+        }
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (incDirs.Checked)
+            {
+                // Count files
+                ShowFiles(fullpath, true);
+            }
+            else
+            {
+                // Count files
+                ShowFiles(fullpath, false);
+            }
+        }
+
+        private void Dir_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = fullpath;
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog
+            {
+                ShowNewFolderButton = true
+            };
+
+            // Show the FolderBrowserDialog.
+
+            DialogResult result = folderDlg.ShowDialog();
+
+            if (result == DialogResult.OK)
+
+            {
+
+                textBox1.Text = folderDlg.SelectedPath;
+
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+
+                fullpath = folderDlg.SelectedPath;
+
+            }
+        }
+    }
+ }
